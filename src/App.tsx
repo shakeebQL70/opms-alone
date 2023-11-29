d486f26c9935488f7fec6b0a165285360d01b30e
Shakeeb Arsalan <shakeebadab50@gmail.com>
2023-11-30 01:40:07 +0530

    public / added

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@
 import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { useProjectStore } from "SUPER/store";
 const App = () => {
-  return <div> Welcome to OPMS </div>
+  const projects = useProjectStore((state: any) => state);
+  console.log({ projects });
+
+  return <div> Welcome to OPMS </div>;
 };
 
 const container = document.getElementById("app");
